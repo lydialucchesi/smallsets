fa0497fc9f2de825cad42b426873273f626a8f46
Lucchesi <Lydia.Lucchesi@anu.edu.au>
2021-04-12 20:37:31 +1000

    revert to one df code

--- R/highlight_changes.R
+++ R/highlight_changes.R
@@ -13,425 +13,156 @@
 #' @importFrom gdata cbindX
 
 
-highlight_changes <- function(list,
-                              constant = "#927C5C",
-                              changed = "cornflowerblue",
-                              added = "#689F38",
-                              deleted = "#b2a38c",
-                              author = NULL,
-                              captionScript = "captions",
-                              captionDir = getwd()) {
-  if (!is.list(constant)) {
-    constantAlpha = .4
-  } else {
-    constantAlpha = constant[[2]]
-    constant = constant[[1]]
-  }
-  
-  if (!is.list(changed)) {
-    changedAlpha = .4
-  } else {
-    changedAlpha = changed[[2]]
-    changed = changed[[1]]
-  }
-  
-  if (!is.list(added)) {
-    addedAlpha = .4
-  } else {
-    addedAlpha = added[[2]]
-    added = added[[1]]
-  }
-  
-  if (!is.list(deleted)) {
-    deletedAlpha = .4
-  } else {
-    deletedAlpha = deleted[[2]]
-    deleted = deleted[[1]]
-  }
-  
-  tileAlphas <-
-    data.frame(
-      colValue = c(constant, changed, added, deleted),
-      alpha = c(constantAlpha, changedAlpha, addedAlpha, deletedAlpha)
-    )
-  
-  write_caption_template(
-    authorName = author,
-    col1 = changed,
-    col2 = added,
-    col3 = deleted,
-    script = captionScript,
-    pathway = captionDir
-  )
-  
-  listInfo <-
-    data.frame(n = seq(1, length(list)), index = as.character(names(list)))
-  listInfo$index <- as.numeric(as.character(listInfo$index))
-  listInfo$snap <-
-    ifelse(floor(listInfo$index) == listInfo$index,
-           listInfo$index,
-           ceiling(listInfo$index))
-  listInfo$first <-
-    ifelse(listInfo$n %in% as.numeric(rownames(unique(
-      data.frame(listInfo)["snap"]
-    ))), TRUE, FALSE)
-  listInfo$ext <- ifelse(grepl("\\.", listInfo$index), FALSE, TRUE)
-  
-  tables <- list()
-  rowDropped <- data.frame()
-  colDropped <- data.frame()
-  colInfo <- data.frame()
-  flag <- FALSE
-  for (p in 1:(length(list) - 1)) {
-    c <- p + 1
+highlight_changes <-
+  function(list,
+           constant = "#927C5C",
+           changed = "cornflowerblue",
+           added = "#689F38",
+           deleted = "#b2a38c",
+           author = NULL,
+           captionScript = "captions",
+           captionDir = getwd()) {
     
-    lprior <- list[[p]]
-    lcurrent <- list[[c]]
-    
-    # ROWNAME ADJUSTMENTS
-    if (isTRUE(flag) & (nrow(lprior) == nrow(lcurrent))) {
-      rownames(lprior) <- rownames(lcurrent)
-    }
-    
-    if ((nrow(lprior) == nrow(lcurrent)) &
-        (FALSE %in% suppressWarnings(rownames(lprior) == rownames(lcurrent)))) {
-      rownames(lprior) <- rownames(lcurrent)
-      # row.names(tprior$body$dataset) <- rownames(lcurrent)
-    }
-    
-    # SET TABLE COLOURS
-    if (p > 1) {
-      tprior <- tables[[p]]
+    if (!is.list(constant)) {
+      constantAlpha = .4
     } else {
-      tprior <- flextable::flextable(lprior)
-      tprior <- flextable::color(tprior, color = constant)
+      constantAlpha = constant[[2]]
+      constant = constant[[1]]
     }
     
-    tcurrent <- flextable::flextable(lcurrent)
-    tcurrent <- flextable::color(tcurrent, color = constant)
-    
-    # what to do if INTPKGRNAME is in tprior
-    if (!(FALSE %in% ((row.names(
-      tprior$body$dataset
-    )[!grepl("INTPKGRNAME", row.names(tprior$body$dataset))]) != rownames(lprior)))) {
-      row.names(tprior$body$dataset) <- rownames(lprior)
+    if (!is.list(changed)) {
+      changedAlpha = .4
+    } else {
+      changedAlpha = changed[[2]]
+      changed = changed[[1]]
     }
-    row.names(tcurrent$body$dataset) <- rownames(lcurrent)
     
-    if (isFALSE(listInfo[listInfo$n == c, "first"]))  {
-      ivals <-
-        row.names(tprior$body$dataset)[row.names(tprior$body$dataset) %in% row.names(lcurrent)]
-      jvals <-
-        colnames(tprior$body$dataset)[colnames(tprior$body$dataset) %in% colnames(lcurrent)]
-      
-      row.names(tprior$body$styles$text$color$data) <-
-        row.names(tprior$body$dataset)
-      
-      if (length(row.names(tprior$body$dataset)) != length(row.names(tcurrent$body$dataset))) {
-        row.names(tcurrent$body$dataset) <-
-          row.names(tprior$body$dataset)[row.names(tprior$body$dataset) %in% row.names(tcurrent$body$dataset)]
-      } else {
-        row.names(tcurrent$body$dataset) <- row.names(tprior$body$dataset)
-      }
-      
-      priorCols <-
-        tprior$body$styles$text$color$data[as.character(ivals), jvals]
-      
-      tcurrent <- flextable::color(
-        tcurrent,
-        color = priorCols,
-        i = (
-          as.character(ivals) == row.names(tcurrent$body$dataset)
-        ),
-        j = jvals
+    if (!is.list(added)) {
+      addedAlpha = .4
+    } else {
+      addedAlpha = added[[2]]
+      added = added[[1]]
+    }
+    
+    if (!is.list(deleted)) {
+      deletedAlpha = .4
+    } else {
+      deletedAlpha = deleted[[2]]
+      deleted = deleted[[1]]
+    }
+    
+    tileAlphas <-
+      data.frame(
+        colValue = c(constant, changed, added, deleted),
+        alpha = c(constantAlpha, changedAlpha, addedAlpha, deletedAlpha)
       )
-    }
     
-    ## ROW DROPS
-    rowsDrop <- setdiff(rownames(lprior), rownames(lcurrent))
-    if (length(rowsDrop) > 0) {
-      tprior <- flextable::color(tprior, color = deleted, i = rowsDrop)
-    }
+    write_caption_template(
+      authorName = author,
+      col1 = changed,
+      col2 = added,
+      col3 = deleted,
+      script = captionScript,
+      pathway = captionDir
+    )
     
-    flag <- FALSE
-    if (length(rowsDrop) > 0) {
-      flag <- TRUE
-    }
-    
-    if (isTRUE(listInfo$first[c])) {
-      oldRowDropped <- rowDropped
-      rowDropped <- data.frame()
-    } else {
-      rowDropped <- rbind(rowDropped, lprior[as.numeric(rowsDrop), ])
-      if (p == (length(list) - 1)) {
-        oldRowDropped <- rowDropped
-      }
-    }
-    
-    ## ROW ADDITIONS
-    rowsAdd <- setdiff(rownames(lcurrent), rownames(lprior))
-    if (length(rowsAdd) > 0) {
-      tcurrent <-
-        flextable::color(tcurrent, color = added, i = rowsAdd)
-    }
-    
-    ## COLUMN DROPS
-    colsDrop <- setdiff(colnames(lprior), colnames(lcurrent))
-    if (length(colsDrop) > 0) {
-      tprior <-
-        flextable::color(tprior,
-                         color = deleted,
-                         j = colsDrop,
-                         part = "all")
-    }
-    
-    if ((isTRUE(listInfo$first[c])) | (c == nrow(listInfo))) {
-      oldColDropped <- colDropped
-      colDropped <- data.frame()
+    tables <- list()
+    for (p in 1:(length(list) - 1)) {
+      c <- p + 1
       
-      oldColInfo <- colInfo
-      colInfo <- data.frame()
-    } else {
-      # use all_of here? get note when running for first time in session
-      new <- select(lprior, colsDrop)
-      if (length(colnames(colDropped)) > 0) {
-        colDropped <- gdata::cbindX(colDropped, new)
+      lprior <- list[[p]]
+      lcurrent <- list[[c]]
+      
+      if (p > 1) {
+        tprior <- tables[[p]]
       } else {
-        colDropped <- new
+        tprior <- flextable::flextable(lprior)
+        tprior <- flextable::color(tprior, color = constant)
       }
-      if (p == (length(list) - 1)) {
-        oldColDropped <- colDropped
-      }
-      new2 <-
-        data.frame(name = colnames(lprior), place = seq(1, length(colnames(lprior))))
       
-      if (nrow(colInfo) > 0) {
-        if (length(colsDrop) > 0) {
-          newColInfo <- subset(new2, name %in% colnames(colDropped))
-          for (s in 1:nrow(newColInfo)) {
-            if (newColInfo$place[s] %in% colInfo$place) {
-              newColInfo$place[s] <- newColInfo$place[s] + 1
-            }
+      tcurrent <- flextable::flextable(lcurrent)
+      tcurrent <- flextable::color(tcurrent, color = constant)
+      
+      rowsDrop <- setdiff(rownames(lprior), rownames(lcurrent))
+      if (length(rowsDrop) > 0) {
+        tprior <- flextable::color(tprior, color = deleted, i = rowsDrop)
+      }
+      
+      rowsAdd <- setdiff(rownames(lcurrent), rownames(lprior))
+      if (length(rowsAdd) > 0) {
+        tcurrent <-
+          flextable::color(tcurrent, color = added, i = rowsAdd)
+      }
+      
+      colsDrop <- setdiff(colnames(lprior), colnames(lcurrent))
+      if (length(colsDrop) > 0) {
+        tprior <-
+          flextable::color(tprior,
+                           color = deleted,
+                           j = colsDrop,
+                           part = "all")
+      }
+      
+      colsAdd <- setdiff(colnames(lcurrent), colnames(lprior))
+      if (length(colsAdd) > 0) {
+        tcurrent <-
+          flextable::color(tcurrent,
+                           color = added,
+                           j = colsAdd,
+                           part = "all")
+      }
+      
+      if (length(rowsDrop) > 0) {
+        lpriorAdj <- subset(lprior, !(row.names(lprior) %in% rowsDrop))
+      } else {
+        lpriorAdj <- lprior
+      }
+      
+      if (length(colsAdd) > 0) {
+        lcurrentAdj <- subset(lcurrent, select = colnames(lprior))
+      } else {
+        lcurrentAdj <- lcurrent
+      }
+      
+      original <-
+        setdiff(subset(lpriorAdj, select = colnames(lcurrentAdj)), lcurrentAdj)
+      update <-
+        setdiff(lcurrentAdj, subset(lpriorAdj, select = colnames(lcurrentAdj)))
+      
+      adjData <- data.frame(r = numeric(), c = numeric())
+      for (i in 1:nrow(original)) {
+        for (j in 1:ncol(original)) {
+          if (identical(original[i, j], update[i, j]) == FALSE) {
+            adjDatum <- data.frame(r = c(row.names(update)[i]), c = c(j))
+            adjData <- rbind(adjData, adjDatum)
           }
-          colInfo <-
-            rbind(colInfo, newColInfo)
-        }
-      } else {
-        colInfo <- subset(new2, name %in% colnames(colDropped))
-      }
-    }
-    
-    ## COLUMN ADDITIONS
-    colsAdd <- setdiff(colnames(lcurrent), colnames(lprior))
-    if (length(colsAdd) > 0) {
-      tcurrent <-
-        flextable::color(tcurrent,
-                         color = added,
-                         j = colsAdd,
-                         part = "body")
-    }
-    
-    ## DATA EDITS
-    if (length(rowsDrop) > 0) {
-      lpriorAdj <- subset(lprior, !(row.names(lprior) %in% rowsDrop))
-    } else {
-      lpriorAdj <- lprior
-    }
-    
-    if (length(colsAdd) > 0) {
-      selectionCols <-
-        colnames(lprior)[colnames(lprior) %in% colnames(lcurrent)]
-      lcurrentAdj <- subset(lcurrent, select = selectionCols)
-    } else {
-      lcurrentAdj <- lcurrent
-    }
-    
-    # original <- setdiff(subset(lpriorAdj, select = colnames(lcurrentAdj)), lcurrentAdj)
-    # update <- setdiff(lcurrentAdj, subset(lpriorAdj, select = colnames(lcurrentAdj)))
-    
-    original <- subset(lpriorAdj, select = colnames(lcurrentAdj))
-    update <- subset(lcurrentAdj, select = colnames(lcurrentAdj))
-    
-    adjData <- data.frame(r = numeric(), c = numeric())
-    for (i in 1:nrow(original)) {
-      for (j in 1:ncol(original)) {
-        if (identical(original[i, j], update[i, j]) == FALSE) {
-          adjDatum <- data.frame(r = c(row.names(update)[i]), c = c(j))
-          adjData <- rbind(adjData, adjDatum)
         }
       }
-    }
-    
-    adjData$r <- as.integer(as.character(adjData$r))
-    
-    if (nrow(adjData) > 0) {
-      for (h in 1:nrow(adjData)) {
+      
+      adjData$r <- as.integer(as.character(adjData$r))
+      # tcurrent <-
+      #   flextable::color(tcurrent,
+      #         color = changed,
+      #         i = adjData$r,
+      #         j = adjData$c)
+      
+      if (nrow(adjData) > 0) {
         tcurrent <-
           flextable::color(
             tcurrent,
             color = changed,
-            i = as.character(adjData$r[h]),
-            j = adjData$c[h]
+            i = (adjData$r == row.names(tcurrent$body$dataset)),
+            j = adjData$c
           )
       }
-    }
-    
-    ## ADD IN DROPPED ROWS AND COLUMNS
-    if (isTRUE(listInfo$ext[p]) & nrow(oldRowDropped) != 0) {
-      savedNames <- row.names(lprior)
-      
-      lcurrentD <- lprior
-      for (d in 1:nrow(oldRowDropped)) {
-        if (row.names(oldRowDropped)[d] > nrow(lcurrentD)) {
-          lcurrentD <-
-            bind_rows(lcurrentD[1:as.numeric(row.names(oldRowDropped))[d] - 1, ],
-                      oldRowDropped[d, ])
-        } else {
-          lcurrentD <-
-            bind_rows(lcurrentD[1:as.numeric(row.names(oldRowDropped))[d] - 1, ],
-                      oldRowDropped[d, ],
-                      lcurrentD[as.numeric(row.names(oldRowDropped))[d]:nrow(lcurrentD), ])
-        }
-      }
-      
-      rows1 <- c()
-      for (r in 1:nrow(lcurrentD)) {
-        if (row.names(lcurrentD)[r] %in% row.names(oldRowDropped)) {
-          rows1 <- c(rows1, paste0("INTPKGRNAME", r))
-        } else {
-          rows1 <- c(rows1, savedNames[1])
-          savedNames <- savedNames[-1]
-        }
-      }
-      row.names(lcurrentD) <- rows1
-      
-      rows2 <- c()
-      priorRows <- row.names(tprior$body$dataset)
-      for (r in 1:nrow(lcurrentD)) {
-        if (grepl("INTPKGRNAME", row.names(lcurrentD)[r])) {
-          rows2 <- c(rows2, row.names(lcurrentD)[r])
-        } else {
-          rows2 <- c(rows2, priorRows[1])
-          priorRows <- priorRows[-1]
-        }
-      }
-      row.names(lcurrentD) <- rows2
-      
-      tcurrentD <- flextable::flextable(lcurrentD)
       
       
-      tcurrentD <- flextable::color(
-        tcurrentD,
-        color = tprior$body$styles$text$color$data,
-        i = row.names(tprior$body$dataset),
-        j = colnames(tprior$body$dataset)
-      )
-      
-      tcurrentD <- flextable::color(
-        tcurrentD,
-        color = deleted,
-        i = grep("INTPKGRNAME", row.names(lcurrentD), value =
-                   TRUE),
-        j = colnames(oldRowDropped)
-      )
-      
-      tcurrentD <- flextable::color(
-        tcurrentD,
-        color = "white",
-        i = grep("INTPKGRNAME", row.names(lcurrentD), value =
-                   TRUE),
-        j = setdiff(colnames(lprior), colnames(oldRowDropped))
-      )
-      
-      # do i need to then save rows as rows1?
-      row.names(tcurrentD$body$dataset) <- rows1
-      
-    }
-    
-    if ((isTRUE(listInfo$ext[p]) &
-         (ncol(oldColDropped) != 0)) |
-        ((p == (length(list) - 1)) & (ncol(oldColDropped) != 0))) {
-      if ((nrow(oldRowDropped) == 0) & (p == (length(list) - 1))) {
-        lcurrentD <- lcurrent
-      }
-      if ((nrow(oldRowDropped) == 0) & (p != (length(list) - 1))) {
-        lcurrentD <- lprior
-      }
-      
-      for (col in 1:nrow(oldColInfo)) {
-        if (col == 1) {
-          lcurrentD <-
-            cbind(lcurrent[, 1:oldColInfo$place[col] - 1],
-                  select(oldColDropped, oldColInfo$name[col]),
-                  lcurrent[, (oldColInfo$place[col]):ncol(lcurrent)])
-        } else {
-          lcurrentD <-
-            cbind(lcurrentD[, 1:oldColInfo$place[col] - 1],
-                  select(oldColDropped, oldColInfo$name[col]),
-                  lcurrentD[, (oldColInfo$place[col]):ncol(lcurrentD)])
-        }
-        
-      }
-      
-      tcurrentD <- flextable::flextable(lcurrentD)
-      
-      tcurrentD <- flextable::color(
-        tcurrentD,
-        color = tcurrent$body$styles$text$color$data,
-        i = row.names(tcurrent$body$dataset),
-        j = colnames(tcurrent$body$dataset)
-      )
-      
-      tcurrentD <- flextable::color(
-        tcurrentD,
-        color = deleted,
-        i = row.names(oldColDropped),
-        j = as.character(oldColInfo$name)
-      )
-      
-      tcurrentD <- flextable::color(
-        tcurrentD,
-        color = "white",
-        i = setdiff(row.names(lcurrent), row.names(oldColDropped)),
-        j = as.character(oldColInfo$name)
-      )
-    }
-    
-    
-    ## SAVING
-    if ((((
-      isTRUE(listInfo$ext[p]) & nrow(oldRowDropped) != 0
-    )) |
-    (((
-      isTRUE(listInfo$ext[p]) &
-      (ncol(oldColDropped) != 0)
-    ) |
-    ((p == (length(list) - 1)) &
-     (ncol(oldColDropped) != 0)
-    )))) & (p == (length(list) - 1))) {
-      tables[[c]] <- tcurrentD
-    } else {
-      tables[[c]] <- tcurrent
-    }
-    
-    if ((isTRUE(listInfo$ext[p]) &
-         (nrow(oldRowDropped) != 0)) |
-        ((isTRUE(listInfo$ext[p]) &
-          (ncol(oldColDropped) != 0)) |
-         ((p == (length(
-           list
-         ) - 1)) & (ncol(oldColDropped) != 0)))) {
-      tables[[p]] <- tcurrentD
-    } else {
       tables[[p]] <- tprior
+      tables[[c]] <- tcurrent
+      
     }
     
-  }
-  
-  tables <- tables[listInfo[listInfo$ext == TRUE, "n"]]
-  
-  return(
-    list(
+    return(list(
       tables,
       constant,
       changed,
@@ -440,6 +171,5 @@ highlight_changes <- function(list,
       captionScript,
       captionDir,
       tileAlphas
-    )
-  )
-}
+    ))
+  }
--- R/select_smallset.R
+++ R/select_smallset.R
@@ -4,22 +4,16 @@
 #' @export
 #' @import "dplyr"
 
-select_smallset <- function(dataNum,
-                            data,
+select_smallset <- function(data,
                             rowCount = 6,
                             rowNums = NULL) {
-  
-  data <- data[[dataNum]]
-  rowCount <- rowCount[[dataNum]]
-  rowNums <- rowNums[[dataNum]]
-  
   if (is.null(rowNums)) {
     smallset <- dplyr::sample_n(data, size = rowCount)
   }
   
   if (!is.null(rowNums)) {
-    smallset <- rbind(data[rowNums,],
-                      dplyr::sample_n(data[-rowNums,], size = (rowCount - length(rowNums))))
+    smallset <- rbind(data[rowNums, ],
+                      dplyr::sample_n(data[-rowNums, ], size = (rowCount - length(rowNums))))
   }
   
   rownames(smallset) <- seq(1, nrow(smallset), 1)
