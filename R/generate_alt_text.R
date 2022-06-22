b18b449dfe2a3135df58868d98642fbde2c0b8b8
Lucchesi <Lydia.Lucchesi@anu.edu.au>
2022-06-22 22:01:11 +1000

    change pkg name for brew command

--- R/generate_alt_text.R
+++ R/generate_alt_text.R
@@ -1,5 +1,5 @@
 #' Generate Alt Text
-#' @description The function identifies produces alt text for the Smallset timeline figure.
+#' @description The function produces alt text for the Smallset Timeline figure.
 #' @keywords internal
 #' @import "brew" "knitr" "readr" "english"
 #' @importFrom plotrix color.id
@@ -16,32 +16,32 @@ generate_alt_text <-
     suppressWarnings(dir.create("altText"))
     
     if ((title != "") & (!subtitle %in% c(" ", ""))) {
-      suppressWarnings(brew(file = system.file("altTextTemplates", "titleOpt1.txt", package="smallset"), output = "altText/intro_1.txt"))
+      suppressWarnings(brew(file = system.file("altTextTemplates", "titleOpt1.txt", package="smallsets"), output = "altText/intro_1.txt"))
     }
     
     if ((title != "") & (subtitle %in% c(" ", ""))) {
-      suppressWarnings(brew(file = system.file("altTextTemplates", "titleOpt2.txt", package="smallset"), output = "altText/intro_1.txt"))
+      suppressWarnings(brew(file = system.file("altTextTemplates", "titleOpt2.txt", package="smallsets"), output = "altText/intro_1.txt"))
     }
     
     if (footnote != "") {
-      suppressWarnings(brew(file = system.file("altTextTemplates", "footnote.txt", package="smallset"), output = "altText/intro_2.txt"))
+      suppressWarnings(brew(file = system.file("altTextTemplates", "footnote.txt", package="smallsets"), output = "altText/intro_2.txt"))
     }
     
-    suppressWarnings(brew(file = system.file("altTextTemplates", "snapNum.txt", package="smallset"), output = "altText/intro_3.txt"))
+    suppressWarnings(brew(file = system.file("altTextTemplates", "snapNum.txt", package="smallsets"), output = "altText/intro_3.txt"))
     
     if (("changed1" %in% row.names(snapshotList[[9]])) | (snapshotList[[6]] %in% snapshotList[[9]]$colValue)) {
       editColour <- sapply(snapshotList[[6]], color.id)[1]
-      suppressWarnings(brew(file = system.file("altTextTemplates", "editColour.txt", package="smallset"), output = "altText/intro_4.txt"))
+      suppressWarnings(brew(file = system.file("altTextTemplates", "editColour.txt", package="smallsets"), output = "altText/intro_4.txt"))
     }
     
     if (("added1" %in% row.names(snapshotList[[9]])) | (snapshotList[[7]] %in% snapshotList[[9]]$colValue)) {
       addColour <- sapply(snapshotList[[7]], color.id)[1]
-      suppressWarnings(brew(file = system.file("altTextTemplates", "addColour.txt", package="smallset"), output = "altText/intro_5.txt"))
+      suppressWarnings(brew(file = system.file("altTextTemplates", "addColour.txt", package="smallsets"), output = "altText/intro_5.txt"))
     }
     
     if (("deleted1" %in% row.names(snapshotList[[9]])) | (snapshotList[[8]] %in% snapshotList[[9]]$colValue)) {
       delColour <- sapply(snapshotList[[8]], color.id)[1]
-      suppressWarnings(brew(file = system.file("altTextTemplates", "delColour.txt", package="smallset"), output = "altText/intro_6.txt"))
+      suppressWarnings(brew(file = system.file("altTextTemplates", "delColour.txt", package="smallsets"), output = "altText/intro_6.txt"))
     }
     
     snaps <- grep("intro", list.files("altText"), value = TRUE)
@@ -69,19 +69,19 @@ generate_alt_text <-
     
     for (i in 1:length(snapshotList[[1]])) {
       
-      suppressWarnings(brew(file = system.file("altTextTemplates", "snapDim.txt", package="smallset"), output = "altText/body_1.txt"))
+      suppressWarnings(brew(file = system.file("altTextTemplates", "snapDim.txt", package="smallsets"), output = "altText/body_1.txt"))
       
       if (i == 1) {
-        suppressWarnings(brew(file = system.file("altTextTemplates", "colNames.txt", package="smallset"), output = "altText/body_2.txt"))
+        suppressWarnings(brew(file = system.file("altTextTemplates", "colNames.txt", package="smallsets"), output = "altText/body_2.txt"))
         
         if (length(altTextInfo$rowsDrop) != 0) {
           if (length(altTextInfo$rowsDrop) > 1) {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "delRows.txt", package="smallset"), output = "altText/body_3.txt")
+              brew(file = system.file("altTextTemplates", "delRows.txt", package="smallsets"), output = "altText/body_3.txt")
             )
             altTextInfo$rowsDrop <- NULL
           } else {
-            suppressWarnings(brew(file = system.file("altTextTemplates", "delRow.txt", package="smallset"), output = "altText/body_4.txt"))
+            suppressWarnings(brew(file = system.file("altTextTemplates", "delRow.txt", package="smallsets"), output = "altText/body_4.txt"))
             altTextInfo$rowsDrop <- NULL
           }
         } else {
@@ -91,12 +91,12 @@ generate_alt_text <-
         if (length(altTextInfo$colsDrop) != 0) {
           if (length(altTextInfo$colsDrop) > 1) {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "delColumns.txt", package="smallset"), output = "altText/body_5.txt")
+              brew(file = system.file("altTextTemplates", "delColumns.txt", package="smallsets"), output = "altText/body_5.txt")
             )
             altTextInfo$colsDrop <- NULL
           } else {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "delColumn.txt", package="smallset"), output = "altText/body_6.txt")
+              brew(file = system.file("altTextTemplates", "delColumn.txt", package="smallsets"), output = "altText/body_6.txt")
             )
             altTextInfo$colsDrop <- NULL
           }
@@ -107,19 +107,19 @@ generate_alt_text <-
         if (isTRUE(abstract) & isTRUE(ghostData)) {
           if (layer_data(l[[i]], 3)$label != "") {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "snapCaption3.txt", package="smallset"), output = "altText/body_13.txt")
+              brew(file = system.file("altTextTemplates", "snapCaption3.txt", package="smallsets"), output = "altText/body_13.txt")
             )
           }
         } else if (isFALSE(abstract) & isFALSE(ghostData)) {
           if (layer_data(l[[i]], 5)$label != "") {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "snapCaption5.txt", package="smallset"), output = "altText/body_13.txt")
+              brew(file = system.file("altTextTemplates", "snapCaption5.txt", package="smallsets"), output = "altText/body_13.txt")
             )
           }
         } else {
           if (layer_data(l[[i]], 4)$label != "") {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "snapCaption4.txt", package="smallset"), output = "altText/body_13.txt")
+              brew(file = system.file("altTextTemplates", "snapCaption4.txt", package="smallsets"), output = "altText/body_13.txt")
             )
           }
         }
@@ -150,11 +150,11 @@ generate_alt_text <-
         if (length(altTextInfo$rowsDrop) != 0) {
           if (length(altTextInfo$rowsDrop) > 1) {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "delRows.txt", package="smallset"), output = "altText/body_3.txt")
+              brew(file = system.file("altTextTemplates", "delRows.txt", package="smallsets"), output = "altText/body_3.txt")
             )
             altTextInfo$rowsDrop <- NULL
           } else {
-            suppressWarnings(brew(file = system.file("altTextTemplates", "delRow.txt", package="smallset"), output = "altText/body_4.txt"))
+            suppressWarnings(brew(file = system.file("altTextTemplates", "delRow.txt", package="smallsets"), output = "altText/body_4.txt"))
             altTextInfo$rowsDrop <- NULL
           }
         } else {
@@ -164,12 +164,12 @@ generate_alt_text <-
         if (length(altTextInfo$colsDrop) != 0) {
           if (length(altTextInfo$colsDrop) > 1) {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "delColumns.txt", package="smallset"), output = "altText/body_5.txt")
+              brew(file = system.file("altTextTemplates", "delColumns.txt", package="smallsets"), output = "altText/body_5.txt")
             )
             altTextInfo$colsDrop <- NULL
           } else {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "delColumn.txt", package="smallset"), output = "altText/body_6.txt")
+              brew(file = system.file("altTextTemplates", "delColumn.txt", package="smallsets"), output = "altText/body_6.txt")
             )
             altTextInfo$colsDrop <- NULL
           }
@@ -180,12 +180,12 @@ generate_alt_text <-
         if (nrow(altTextInfo$adjData) > 0) {
           if (nrow(altTextInfo$adjData) > 1) {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "editCells.txt", package="smallset"), output = "altText/body_7.txt")
+              brew(file = system.file("altTextTemplates", "editCells.txt", package="smallsets"), output = "altText/body_7.txt")
             )
             altTextInfo$adjData <- NULL
           } else {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "editCell.txt", package="smallset"), output = "altText/body_8.txt")
+              brew(file = system.file("altTextTemplates", "editCell.txt", package="smallsets"), output = "altText/body_8.txt")
             )
             altTextInfo$adjData <- NULL
           }
@@ -196,12 +196,12 @@ generate_alt_text <-
         if (length(altTextInfo$rowsAdd) != 0) {
           if (length(altTextInfo$rowsAdd) > 1) {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "addRows.txt", package="smallset"), output = "altText/body_9.txt")
+              brew(file = system.file("altTextTemplates", "addRows.txt", package="smallsets"), output = "altText/body_9.txt")
             )
             altTextInfo$rowsAdd <- NULL
           } else {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "addRow.txt", package="smallset"), output = "altText/body_10.txt")
+              brew(file = system.file("altTextTemplates", "addRow.txt", package="smallsets"), output = "altText/body_10.txt")
             )
             altTextInfo$rowsAdd <- NULL
           }
@@ -212,12 +212,12 @@ generate_alt_text <-
         if (length(altTextInfo$colsAdd) != 0) {
           if (length(altTextInfo$colsAdd) > 1) {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "addColumns.txt", package="smallset"), output = "altText/body_11.txt")
+              brew(file = system.file("altTextTemplates", "addColumns.txt", package="smallsets"), output = "altText/body_11.txt")
             )
             altTextInfo$colsAdd <- NULL
           } else {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "addColumn.txt", package="smallset"), output = "altText/body_12.txt")
+              brew(file = system.file("altTextTemplates", "addColumn.txt", package="smallsets"), output = "altText/body_12.txt")
             )
             altTextInfo$colsAdd <- NULL
           }
@@ -230,19 +230,19 @@ generate_alt_text <-
         if (isTRUE(abstract) & isTRUE(ghostData)) {
           if (layer_data(l[[i]], 3)$label != "") {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "snapCaption3.txt", package="smallset"), output = "altText/body_13.txt")
+              brew(file = system.file("altTextTemplates", "snapCaption3.txt", package="smallsets"), output = "altText/body_13.txt")
             )
           }
         } else if (isFALSE(abstract) & isFALSE(ghostData)) {
           if (layer_data(l[[i]], 5)$label != "") {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "snapCaption5.txt", package="smallset"), output = "altText/body_13.txt")
+              brew(file = system.file("altTextTemplates", "snapCaption5.txt", package="smallsets"), output = "altText/body_13.txt")
             )
           }
         } else {
           if (layer_data(l[[i]], 4)$label != "") {
             suppressWarnings(
-              brew(file = system.file("altTextTemplates", "snapCaption4.txt", package="smallset"), output = "altText/body_13.txt")
+              brew(file = system.file("altTextTemplates", "snapCaption4.txt", package="smallsets"), output = "altText/body_13.txt")
             )
           }
         }
